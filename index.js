6496b226967b8efc8c2ce7d4a9212f6729e06846
LanChi <lpham2525@gmail.com>
2021-06-25 15:13:42 -0400

    added product page

--- index.js
+++ index.js
@@ -32,9 +32,49 @@ let $grid = $(".grid").isotope({
 
 //filter items on button click 
 $(".button-group").on("click","button", function() {
-  let filterVale=$(this).attr("data-filter")
+  let filterValue=$(this).attr("data-filter")
   $grid.isotope({filter: filterValue})
 })
 
+  //new phones owl carousel
+  $("#new-phones.owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    }
+  })
+
+  // product quantity section
+  let $qty_up=$(".qty.qty-up")
+  let $qty_down = $(".qty.qty-down")
+  let $input = $("qty.qty_input")
+
+  //click on qty up button
+  $qty_up.click(function(event) {
+    if($input.val()>=1 && $input.val()<=9) {
+      $input.val(function(i, oldValue){
+        return ++oldValue
+      })
+    }
+  })
+
+  //click on qty down button
+  $qty_down.click(function (event) {
+    if ($input.val() > 1 && $input.val() <= 10) {
+      $input.val(function (i, oldValue) {
+        return --oldValue
+      })
+    }
+  })
 })
 
